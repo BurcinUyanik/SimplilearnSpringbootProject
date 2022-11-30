04438f178e987488c5ca10ecc887e1ad3006021f
BurcinUyanik <burcin.uyanik@hotmail.com>
2022-11-30 23:23:51 +0000

    new

---
+++ src/main/java/com/simplilearn/burcin/BurcinApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.burcin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurcinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurcinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/burcin/BurcinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.burcin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurcinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
